32ad81046d29f87b7a5779368df2bf5590287d6b
Vidit <vdsavaliya19@gmail.com>
2022-06-25 10:49:20 +0530

    25/06/2022
    25/06/2022

--- lib/screen/cart/view/cartScreen.dart
+++ lib/screen/cart/view/cartScreen.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerce/screen/cart/controller/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+import '../../../utils/constant/size.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -10,10 +14,47 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(),
-      ),
+    return GetBuilder<Cart>(
+      init: Cart(),
+      builder: (controller) {
+
+        print("=======    ${controller.cartList.length}");
+
+        return SafeArea(
+          child: Scaffold(
+            body: Container(
+              alignment: Alignment.center,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      elevation: 3,
+                      child: Row(
+                        children: [
+                          Container(
+                              height: 100,
+                              width: 100,
+                              child:
+                                  Image.asset(controller.cartList[index].image!)),
+                          W(20),
+                          Text("${controller.cartList[index].name!}"),
+                          Icon(Icons.add_circle_outline),
+                          W(10),
+                          Icon(Icons.minimize),
+                          W(10),
+                          Icon(Icons.delete),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: controller.cartList.length,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/utils/constant/size.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget W(double w) {
+  return SizedBox(width: w);
+}
+
+Widget H(double h) {
+  return SizedBox(width: h);
+}
